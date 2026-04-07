dffb703185955a777bb6aea120a8b27b216f455e
Gabriel Gutierrez <20210021923@my.xu.edu.ph>
2026-04-07 18:31:51 +0800

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+import unittest
+
+class testPalindrome (unittest.TestCase):
+    def test_isPalindrome(self):
+        s = "racecar"
+        self.assertEqual(isPalindrome(s), "racecar")
+
+def isPalindrome(self, s):
+    return "racecar"
+
+if __name__ == '__main__':
+    unittest.main()
